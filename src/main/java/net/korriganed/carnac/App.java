de2ca69a4feb41d007cd9f899c406287d14b0d36
chat-wane <grumpy.chat.wane@gmail.com>
2016-12-06 18:58:32 +0100

    reading part of an image

--- src/main/java/net/korriganed/carnac/App.java
+++ src/main/java/net/korriganed/carnac/App.java
@@ -1,13 +1,36 @@
 package net.korriganed.carnac;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("start...");
+		BufferedImage image = null;
+		try {
+			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+			String file = classloader.getResource("mnist_train0.jpg").getFile();
+			image = ImageIO.read(new File(file));
+			BufferedImage small = image.getSubimage(0, 0, 25, 25);
+			ArrayList<Integer> pixels = new ArrayList<>();
+			for (int row = 0; row < small.getWidth(); ++row) {
+				for (int column = 0; column < small.getHeight(); ++column) {
+					int pixel = small.getRGB(row, column);
+					int shadeOfgrey = (pixel >> 16) & 0xff;
+					pixels.add(shadeOfgrey);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
